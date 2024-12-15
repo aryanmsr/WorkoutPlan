6f2c69a10e52f99345ecff1a5565b0eab68920bd
aryanmsr <aryanmsr@gmail.com>
2024-12-15 13:18:03 -0500

    modifying config

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ PROMPT_TEMPLATE_PATH = './data/prompt_template.txt'
 ACTIVITY_DATA_PATH = './data/processed_run_data.json'
 SUMMARY_STATS_PATH = './data/summary_statistics.json'
 MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.3"
+
